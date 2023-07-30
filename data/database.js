7d2f7d57ee2936f5988ada3b4785ffe9df0622eb
Sudhanshu <sudhanshukumar4534@gmail.com>
2023-07-30 15:24:51 +0530

    initial commit

---
+++ data/database.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+
+export const connectDB = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+    dbName: "backendAPI"
+}).then(()=>console.log("Database Connected")).catch((e)=>console.log(e));
+}
